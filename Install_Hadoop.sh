4d176b0f7257131012372f6eaf1c537210b1a6c3
ShreyasK2411 <114290670+ShreyasK2411@users.noreply.github.com>
2022-12-25 14:54:08 +0530

    Add files via upload

--- Install_Hadoop.sh
+++ Install_Hadoop.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
 
-# Step 1: Install java
 sudo apt-get -y install openjdk-8-jdk-headless
 
-# Step 2: Install Hadoop
 mkdir server
 cd server
 wget  https://archive.apache.org/dist/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz
 tar xvzf hadoop-2.7.3.tar.gz
 
-# Step 3: Setup Java_Home
 cd ~
 sed -i 's+export JAVA_HOME=${JAVA_HOME}+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64+g' ~/server/hadoop-2.7.3/etc/hadoop/hadoop-env.sh
 
-# Edit the core-site.xml file
 echo Enter dns of namenode: 
 read namenode_dns
-sed -i 's+<configuration>\
-	  </configuration>+\
-<configuration>\
-  <property>\
-    <name>fs.defaultFS</name>\
-    <value>$namenode_dns:9000</value>\
-  </property>\
-</configuration>+g' ~/server/hadoop-2.7.3/etc/hadoop/core-site.xml
+sed -i 's/<nnode>/$namenode_dns/g' core-site.xml
+mv core-site.xml ~/server/hadoop-2.7.3/etc/hadoop/
 
-
-# Creating a data directory
 sudo mkdir -p /usr/local/hadoop/hdfs/data
 sudo chown -R ubuntu:ubuntu /usr/local/hadoop/hdfs/data
